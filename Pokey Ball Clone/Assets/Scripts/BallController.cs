bb4b818d822b4243f06dba3cd0ef6a16c41b865b
enJJ <burak.ulusahan@ogr.sakarya.edu.tr>
2020-07-01 21:14:35 +0300

    Setup cinemachine settings and try to feel jump mechanic

--- Pokey Ball Clone/Assets/Scripts/BallController.cs
+++ Pokey Ball Clone/Assets/Scripts/BallController.cs
@@ -7,6 +7,8 @@ public class BallController : MonoBehaviour{
 
     [SerializeField]
     private float _forcePower = 0;
+    [SerializeField]
+    private float _gravityMultiplier= 0;
     #endregion
 
 
@@ -17,6 +19,7 @@ public class BallController : MonoBehaviour{
     }
 
     private void Update() {
+        ApplyConstantFallForce();
         if (Input.GetKeyDown(KeyCode.Space)) {
             _rb.AddForce(Vector3.up * _forcePower,ForceMode.Impulse);
         }
@@ -26,8 +29,11 @@ public class BallController : MonoBehaviour{
 
 
 
-    #region MonoBehaviour
+    #region Helper Methods
 
+    private void ApplyConstantFallForce() {
+        _rb.AddForceAtPosition(Vector3.down * _gravityMultiplier, transform.position);
+    }
 
     #endregion
 
