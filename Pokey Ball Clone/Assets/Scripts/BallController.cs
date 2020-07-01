87776bd295159971164434ee6932e1086a63c485
enJJ <burak.ulusahan@ogr.sakarya.edu.tr>
2020-07-01 19:00:22 +0300

    Design simple scene

---
+++ Pokey Ball Clone/Assets/Scripts/BallController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class BallController : MonoBehaviour{
+
+    #region Variables
+    private Rigidbody _rb;
+
+    [SerializeField]
+    private float _forcePower = 0;
+    #endregion
+
+
+
+    #region MonoBehaviour
+    private void Start() {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            _rb.AddForce(Vector3.up * _forcePower,ForceMode.Impulse);
+        }
+    }
+
+    #endregion
+
+
+
+    #region MonoBehaviour
+
+
+    #endregion
+
+}
+
